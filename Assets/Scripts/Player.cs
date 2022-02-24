4cafbbe74badbc12c4228ca480a73dae1da96827
thomasfxx <thomasfox2712@gmail.com>
2022-02-24 11:17:56 +0000

    bugfixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float jumpHeight = 5f;
+    [SerializeField] private float doubleJumpHeight = 7f;
+    [SerializeField] private float speed = 2f;
+
     bool jumpKeyPressed;
+    int doubleJump = 0 ;
     float horizontalInput;
     Rigidbody rigidbodyComponent;
+    
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +31,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            jumpKeyPressed = true;
-            
+            jumpKeyPressed = true;  
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
@@ -31,16 +42,43 @@ public class Player : MonoBehaviour
     //Something to do with Physics Update (Makes sure Physics runs in identical manner, regardless of the computer power)
     void FixedUpdate()
     {
-        if(jumpKeyPressed)
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * speed, rigidbodyComponent.velocity.y, 0);
+        
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length > 0)
         {
-            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
-            jumpKeyPressed = false;
+            doubleJump = 0;
+        }
+
+
+        if (jumpKeyPressed)
+        {
+            if(doubleJump == 1)
+            {
+                rigidbodyComponent.AddForce(Vector3.up * doubleJumpHeight, ForceMode.VelocityChange);
+                doubleJump = 0;
+            }
+
+            if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length > 0)
+            {
+                rigidbodyComponent.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+                jumpKeyPressed = false;
+                doubleJump = 1;
+
+            }
+
 
         }
 
-        rigidbodyComponent.velocity = new Vector3(horizontalInput * 1.5f, rigidbodyComponent.velocity.y, 0);
+
 
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
 }
